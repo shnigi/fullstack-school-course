6072df276db134718c3f5b3dfceae21a7d56b213
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-01-18 17:47:08 +0200

    1.7

--- 1/src/index.js
+++ 1/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistiikka = ({statistiikka}) => (
-  <div>
-    <h2>Statistiikka</h2>
-    <p>Hyvä: {statistiikka.hyva}</p>
-    <p>Neutraali: {statistiikka.neutraali}</p>
-    <p>Huono: {statistiikka.huono}</p>
-  </div>
-)
+
+const Statistiikka = ({statistiikka}) => {
+  const number = statistiikka.hyva - statistiikka.huono;
+  const sum = statistiikka.hyva + statistiikka.neutraali + statistiikka.huono;
+  const average = number / sum;
+  const positiivisia = statistiikka.hyva / sum;
+  return (
+    <div>
+      <h2>Statistiikka</h2>
+      <p>Hyvä: {statistiikka.hyva}</p>
+      <p>Neutraali: {statistiikka.neutraali}</p>
+      <p>Huono: {statistiikka.huono}</p>
+      <p>Keskiarvo: {average.toFixed(2)}</p>
+      <p>Positiivisia: {(positiivisia*100).toFixed(2)}%</p>
+    </div>
+  )
+}
 
 class App extends React.Component {
   constructor() {

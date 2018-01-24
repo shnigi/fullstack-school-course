6d550fce3aeebac6b61b9f88bee915bbc8980c6e
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-01-24 14:33:49 +0200

    2.3

--- 2/src/components/Yhteensa.js
+++ 2/src/components/Yhteensa.js
@@ -1,7 +1,10 @@
 import React from 'react'
 
-const Yhteensa = ({kurssi}) => (
-  <p>yhteensä {kurssi.osat[0].tehtavia + kurssi.osat[1].tehtavia + kurssi.osat[2].tehtavia} tehtävää</p>
-)
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+const Yhteensa = ({kurssi}) => {
+  const tehtavia = kurssi.osat.map(kurssi => kurssi.tehtavia);
+  return (<p>Yhteensä {tehtavia.reduce(reducer)} tehtävää</p>)
+};
 
 export default Yhteensa

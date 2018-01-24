8f8615b75f0f3578e72e76bc3507636b9c1b6130
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-01-24 18:03:44 +0200

    2.9

--- 2/src/index.js
+++ 2/src/index.js
@@ -8,10 +8,26 @@ class App extends React.Component {
       persons: [{
         name: 'Niki Ahlskog',
         id: 1
+      },
+      {
+        name: 'Seppo Rajamäki',
+        id: 2,
+        phone: '345343452'
+      },
+      {
+        name: 'Hannu Karpo',
+        id: 3,
+        phone: '2352523523'
       }],
       newName: '',
-      phoneNumber: ''
+      phoneNumber: '',
+      filter: '',
+      filteredItems: []
     }
+  }
+
+  componentWillMount = () => {
+    this.setState({filteredItems: this.state.persons})
   }
 
   handleNewName = (event) => {
@@ -45,17 +61,38 @@ class App extends React.Component {
       this.setState({
         persons,
         newName: '',
-        phoneNumber: ''
+        phoneNumber: '',
+        filteredItems: persons
       })
     } else {
       alert('Nimi on jo listalla!')
     }
   }
 
+  filterList = (event) => {
+     let updatedList = this.state.persons;
+     updatedList = updatedList.filter((person) => {
+       return person.name.toLowerCase().search(
+         event.target.value.toLowerCase()) !== -1;
+     });
+     this.setState({filteredItems: updatedList});
+     this.setState({filter: event.target.value});
+   }
+
   render() {
     return (
       <div>
         <h2>Puhelinluettelo</h2>
+
+        <div>
+         Rajaa näytettäviä:
+         <input
+            value={this.state.filter}
+            onChange={this.filterList}
+           />
+        </div>
+        <br />
+
         <form onSubmit={this.addPerson}>
           <div>
             nimi:
@@ -76,7 +113,7 @@ class App extends React.Component {
           </div>
         </form>
         <h2>Numerot</h2>
-        {this.state.persons.map(person => <p key={person.id}>{person.name} {person.phone}</p>)}
+        {this.state.filteredItems.map(person => <p key={person.id}>{person.name} {person.phone}</p>)}
       </div>
     )
   }

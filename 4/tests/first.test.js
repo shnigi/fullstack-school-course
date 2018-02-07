9d4aeccd9b684e7be18f1e5daea7f131abcc23fe
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-02-07 13:18:58 +0200

    4.4

--- 4/tests/first.test.js
+++ 4/tests/first.test.js
@@ -6,3 +6,30 @@ test('dummy is called', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+{
+"_id": "5a74d63f639c943b14e9da25",
+"title": "Harry Portter",
+"author": "Geijo",
+"url": "google.com",
+"likes": 4,
+"__v": 0
+},
+{
+"_id": "5a7adbe9e65bc32ff8ffb940",
+"title": "Testikamaa",
+"author": "Jokujambba",
+"url": "test.com",
+"likes": 2,
+"__v": 0
+}
+]
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(6)
+  })
+})

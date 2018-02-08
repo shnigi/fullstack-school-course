be93f0f05158bb3c0da9b7c7c73c3c4f359d0085
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-02-08 15:47:16 +0200

    4.16 api

--- 4/controllers/users.js
+++ 4/controllers/users.js
@@ -2,22 +2,33 @@ const bcrypt = require('bcrypt-nodejs')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+const userIsUnique = async (username) => {
+  const response = await User.find({username})
+  return response.length === 0 ? true : false;
+}
+
 usersRouter.post('/', async (request, response) => {
   try {
     const body = request.body
-    bcrypt.hash(body.password, null, null, async (err, passwordHash) => {
+    const uniqueUser = await userIsUnique(body.username);
+    if (body.password.length > 3 && uniqueUser) {
+      bcrypt.hash(body.password, null, null, async (err, passwordHash) => {
 
-      const user = new User({
-        username: body.username,
-        name: body.name,
-        passwordHash,
-        adult: body.adult
+        const user = new User({
+          username: body.username,
+          name: body.name,
+          passwordHash,
+          adult: body.adult ? body.adult : true
+        })
+
+        const savedUser = await user.save()
+
+        response.json(User.format(savedUser))
       })
+    } else {
+      response.status(409).json({ error: 'Username is not unique or password is too short' })
+    }
 
-      const savedUser = await user.save()
-
-      response.json(User.format(savedUser))
-    })
   } catch (exception) {
     console.log(exception)
     response.status(500).json({ error: 'something went wrong...' })

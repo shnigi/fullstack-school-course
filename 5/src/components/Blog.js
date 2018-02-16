796964c5180e0289fd37c4b1e7a3db29d7b6f3bb
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-02-16 15:02:11 +0200

    5.6

--- 5/src/components/Blog.js
+++ 5/src/components/Blog.js
@@ -1,8 +1,72 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log('props', props);
+    this.state = {
+      visible: false
+    }
+  }
+
+  toggleVisibility = () => {
+    this.setState({visible: !this.state.visible})
+  }
+
+  render() {
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+
+    const hideWhenVisible = { display: this.state.visible ? 'none' : '' }
+    const showWhenVisible = { display: this.state.visible ? '' : 'none' }
+
+    return (
+      <div style={blogStyle}>
+        <h3 onClick={this.toggleVisibility}>{this.props.blog.title} {this.props.blog.author}</h3>
+        <div style={showWhenVisible}>
+          {this.props.blog.id}
+          <br />
+          {this.props.blog.author}
+          <br />
+          {this.props.blog.likes}
+          <br />
+          {this.props.blog.title}
+          <br />
+          {this.props.blog.url}
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Blog
+
+// import React from 'react'
+// const Blog = ({blog}) => {
+//   console.log('blog', blog);
+//
+//   const ShowInfo = (blog) => {
+//     console.log('hello', blog);
+//     return (
+//       <div>
+//       {blog.id}
+//       {blog.author}
+//       {blog.likes}
+//       {blog.title}
+//       {blog.url}
+//       </div>
+//     )
+//   }
+//
+//   return (
+//   <div onClick={() => ShowInfo(blog)}>
+//     {blog.title} {blog.author}
+//     <ShowInfo/>
+//   </div>
+// )}
+//
+// export default Blog

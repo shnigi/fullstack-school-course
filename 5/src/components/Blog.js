d478641a9a37d2b04e483dc478fe7f1e49b56904
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-02-18 19:05:45 +0200

    remove console log

--- 5/src/components/Blog.js
+++ 5/src/components/Blog.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 
 class Blog extends React.Component {
   constructor(props) {
-    console.log('PROPSIT', props);
     super(props)
     this.state = {
       visible: false

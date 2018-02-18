19009bc2b6aa287e3d9869c5cbf847d518a81ed0
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-02-18 18:00:34 +0200

    5.13

--- 5/src/components/Blog.test.js
+++ 5/src/components/Blog.test.js
@@ -11,9 +11,33 @@ describe.only('<SimpleBlog />', () => {
     }
 
     const blogComponent = shallow(<SimpleBlog blog={blog} />)
-    console.log('Component debug', blogComponent.debug())
+    // console.log('Component debug', blogComponent.debug())
     const contentDiv = blogComponent.find('.content')
 
     expect(contentDiv.text()).toContain(blog.title, blog.author)
   })
+
+  it('clicking the button calls event handler twice', () => {
+    const blog = {
+      title: 'Komponenttitestaus tapahtuu jestillä ja enzymellä',
+      author: 'sneen',
+      likes: 4
+    }
+
+    const mockHandler = jest.fn()
+
+    const blogComponent = shallow(
+      <SimpleBlog
+        blog={blog}
+        onClick={mockHandler}
+      />
+    )
+
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
+
 })

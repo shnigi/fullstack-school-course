d110e1e5656a014c04aad69266ebc963f3a51e4c
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-03-01 13:30:18 +0200

    osa 6.1

---
+++ redux-anecdotes-v2/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "extends": ["react-app", "plugin:jsx-a11y/recommended"],
+    "plugins": ["jsx-a11y"],
+    "parserOptions": {
+        "ecmaVersion": 9,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true,
+            "experimentalObjectRestSpread": true
+        }
+    },
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "jsx-a11y/href-no-hash": "off",
+        "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }]
+    }
+};
